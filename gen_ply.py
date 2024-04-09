246595d9aec0a5a362c7b85820997af8e1a60635
timsong <timsong@seas.upenn.edu>
2024-04-09 03:17:47 +0000

    generate ply done

--- gen_ply.py
+++ gen_ply.py
@@ -5,12 +5,35 @@ from scipy.spatial.transform import Rotation as R
 from plyfile import PlyData, PlyElement
 import json
 
+def quat2mat(quat: np.ndarray):
+    # convert quaternion to rotation matrix
+    w, x, y, z = quat
+    return np.array([[1 - 2*y*y - 2*z*z, 2*x*y - 2*z*w, 2*x*z + 2*y*w],
+                     [2*x*y + 2*z*w, 1 - 2*x*x - 2*z*z, 2*y*z - 2*x*w],
+                     [2*x*z - 2*y*w, 2*y*z + 2*x*w, 1 - 2*x*x - 2*y*y]])
+
+def mat2quat(mat: np.ndarray):
+    # convert rotation matrix to quaternion
+    
+    if 1 + mat[0, 0] + mat[1, 1] + mat[2, 2] < 1e-6:
+        x = np.sqrt(1+mat[0, 0] - mat[1, 1] - mat[2, 2]) / 2
+        y = (mat[0, 1] + mat[1, 0]) / (4*x)
+        z = (mat[0, 2] + mat[2, 0]) / (4*x)
+        w = (mat[1, 2] - mat[2, 1]) / (4*x)
+    else:
+        w = np.sqrt(1 + mat[0, 0] + mat[1, 1] + mat[2, 2]) / 2
+        x = (mat[2, 1] - mat[1, 2]) / (4*w)
+        y = (mat[0, 2] - mat[2, 0]) / (4*w)
+        z = (mat[1, 0] - mat[0, 1]) / (4*w)
+    return np.array([w, x, y, z])
+
 def normal2quat(normals):
     # the last column of the rotation matrix is the normal, other two axis are arbitrary
     normals = normals / np.linalg.norm(normals, axis=1)[:, None]
     norm1 = []
     norm2 = []
     all_quat = []
+    max_err = -100
     for normal in normals:
         # randomly find a vector that is vertical to the normal
         v1 = np.random.rand(3)*2 - 1.0
@@ -20,10 +43,20 @@ def normal2quat(normals):
         v2 = np.cross(normal, v1)
         norm2.append(v2)
         # create rotation matrix
-        rot = np.array([v1, v2, normal]).T  
+        rot_mat = np.array([v1, v2, normal]).T  
+        
         # convert to quaternion
-        quat = R.from_matrix(rot).as_quat()
+        # rot = R.from_matrix(rot_mat)
+        # quat = rot.as_quat()
+        quat = mat2quat(rot_mat)
+        
         all_quat.append(quat)
+        mat = quat2mat(quat)
+        err = np.sum(np.abs(rot_mat - mat))
+        print("Error: ", err)
+        if err > max_err:
+            max_err = err
+    print("Max Error: ", max_err)
     return np.array(all_quat), np.array(norm1), np.array(norm2)
 
 
@@ -43,8 +76,8 @@ def gen_ply():
     
     non_edge_mask = ((abs(mesh.vertices[:, 0]) + abs(mesh.vertices[:, 1]))!= 1) & (abs(mesh.vertices[:, 0])+abs(mesh.vertices[:, 2])!= 1) & (abs(mesh.vertices[:, 1])+abs(mesh.vertices[:, 2])!= 1)
 
-    # select_id = np.random.choice(sum(non_edge_mask), 100)
-    select_id = [i for i in range(0, 1000, 10)]
+    select_id = np.random.choice(sum(non_edge_mask), 200)
+    # select_id = [i for i in range(0, 1000, 5)]
 
     select_vert = mesh.vertices[non_edge_mask][select_id]
     select_norm = mesh.vertex_normals[non_edge_mask][select_id]
@@ -70,22 +103,27 @@ def gen_ply():
     z_face = select_vert[:, 2] == select_vert[:, 2].min()
     y_face = select_vert[:, 1] == select_vert[:, 1].min()
     x_face = select_vert[:, 0] == select_vert[:, 0].min()
+    zz_face = select_vert[:, 2] == select_vert[:, 2].max()
+    yy_face = select_vert[:, 1] == select_vert[:, 1].max()
+    xx_face = select_vert[:, 0] == select_vert[:, 0].max()
 
     xyz = np.array(select_vert) * 20
     normals = np.array(select_norm)
     # generate color in N * [0, 1]
-    f_dc = np.random.uniform(0, 1, (len(xyz), 3))
+    # f_dc = np.random.uniform(0, 1, (len(xyz), 3))
+    f_dc = np.zeros((len(xyz), 3))
 
-    f_dc[x_face] *= 0.1
-    f_dc[x_face, 0] = 1
-    f_dc[y_face] *= 0.1
-    f_dc[y_face, 1] = 1
-    f_dc[z_face] *= 0.1
-    f_dc[z_face, 2] = 1
+    f_dc[z_face] = [1, 0, 0]
+    f_dc[y_face] = [0, 1, 0]
+    f_dc[x_face] = [0, 0, 1]
+    f_dc[zz_face] = [1, 1, 0]
+    f_dc[yy_face] = [1, 0, 1]
+    f_dc[xx_face] = [0, 1, 1]
+
 
     f_rest = np.zeros((len(xyz), 45))
     opacities = np.ones((len(xyz), 1))*10
-    scale = np.random.uniform(0.001, 0.1, (len(xyz), 3))
+    scale = np.random.uniform(0.01, 0.1, (len(xyz), 3))
     scale[:, 2] = 0.01
     # generate quaternion based on normal
     rotation, norm1, norm2 = normal2quat(normals)
