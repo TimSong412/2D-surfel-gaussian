c943551170b3bc8d836c364513dfb56694f5e276
timsong <timsong@seas.upenn.edu>
2024-04-10 04:56:06 +0000

    gaussian scale exp

--- gen_ply.py
+++ gen_ply.py
@@ -4,6 +4,7 @@ from wis3d import Wis3D
 from scipy.spatial.transform import Rotation as R
 from plyfile import PlyData, PlyElement
 import json
+from matplotlib import pyplot as plt
 
 def quat2mat(quat: np.ndarray):
     # convert quaternion to rotation matrix
@@ -16,10 +17,18 @@ def mat2quat(mat: np.ndarray):
     # convert rotation matrix to quaternion
     
     if 1 + mat[0, 0] + mat[1, 1] + mat[2, 2] < 1e-6:
-        x = np.sqrt(1+mat[0, 0] - mat[1, 1] - mat[2, 2]) / 2
-        y = (mat[0, 1] + mat[1, 0]) / (4*x)
-        z = (mat[0, 2] + mat[2, 0]) / (4*x)
-        w = (mat[1, 2] - mat[2, 1]) / (4*x)
+        # w = 0
+        if 1+mat[0, 0] - mat[1, 1] - mat[2, 2] < 1e-6:
+            # x = 0
+            y = np.sqrt(1-mat[0, 0] + mat[1, 1] - mat[2, 2]) / 2
+            x = (mat[0, 1] + mat[1, 0]) / (4*y)
+            z = (mat[1, 2] + mat[2, 1]) / (4*y)
+            w = (mat[2, 0] - mat[0, 2]) / (4*y)
+        else:
+            x = np.sqrt(1+mat[0, 0] - mat[1, 1] - mat[2, 2]) / 2
+            y = (mat[0, 1] + mat[1, 0]) / (4*x)
+            z = (mat[0, 2] + mat[2, 0]) / (4*x)
+            w = (mat[1, 2] - mat[2, 1]) / (4*x)
     else:
         w = np.sqrt(1 + mat[0, 0] + mat[1, 1] + mat[2, 2]) / 2
         x = (mat[2, 1] - mat[1, 2]) / (4*w)
@@ -53,7 +62,7 @@ def normal2quat(normals):
         all_quat.append(quat)
         mat = quat2mat(quat)
         err = np.sum(np.abs(rot_mat - mat))
-        print("Error: ", err)
+        # print("Error: ", err)
         if err > max_err:
             max_err = err
     print("Max Error: ", max_err)
@@ -61,9 +70,13 @@ def normal2quat(normals):
 
 
 
-def gen_ply():
+def gen_cube():
     # generate a cube gaussian spalting ply file
     mesh = trimesh.creation.box((1, 1, 1))
+    mesh = mesh.subdivide()
+    mesh = mesh.subdivide()
+    mesh = mesh.subdivide()
+    mesh = mesh.subdivide()
     mesh = mesh.subdivide()
     mesh = mesh.subdivide()
     mesh = mesh.subdivide()
@@ -76,7 +89,7 @@ def gen_ply():
     
     non_edge_mask = ((abs(mesh.vertices[:, 0]) + abs(mesh.vertices[:, 1]))!= 1) & (abs(mesh.vertices[:, 0])+abs(mesh.vertices[:, 2])!= 1) & (abs(mesh.vertices[:, 1])+abs(mesh.vertices[:, 2])!= 1)
 
-    select_id = np.random.choice(sum(non_edge_mask), 200)
+    select_id = np.random.choice(sum(non_edge_mask), 100000, replace=False)
     # select_id = [i for i in range(0, 1000, 5)]
 
     select_vert = mesh.vertices[non_edge_mask][select_id]
@@ -86,8 +99,8 @@ def gen_ply():
     v3d = Wis3D("dbg", "vis_normal", "xyz")
     v3d.add_mesh(mesh, "cube")
     # add vertex normal
-    
-    v3d.add_lines(np.array(select_vert), np.array(select_vert + select_norm*0.1), name=f"normals")
+    short_norm = select_norm * 0.01
+    v3d.add_lines(np.array(select_vert), np.array(select_vert + short_norm), name=f"normals")
 
     # create gaussian splatting ply
     print("Create Gaussian Splatting Ply")
@@ -107,7 +120,7 @@ def gen_ply():
     yy_face = select_vert[:, 1] == select_vert[:, 1].max()
     xx_face = select_vert[:, 0] == select_vert[:, 0].max()
 
-    xyz = np.array(select_vert) * 20
+    xyz = np.array(select_vert) * 30
     normals = np.array(select_norm)
     # generate color in N * [0, 1]
     # f_dc = np.random.uniform(0, 1, (len(xyz), 3))
@@ -120,11 +133,40 @@ def gen_ply():
     f_dc[yy_face] = [1, 0, 1]
     f_dc[xx_face] = [0, 1, 1]
 
+    # generate grid texture for the cube
+    freq = 10.0
+    grid_length = (select_vert[:, 0].max() - select_vert[:, 0].min()) / freq
+    vertmax = abs(select_vert[:, 0]).max()
+    for idx, vert in enumerate(select_vert):
+        x = vert[0]
+        y = vert[1]
+        z = vert[2]
+        if abs(x) == vertmax:
+            abs_z = z - select_vert[:, 2].min()
+            pos_z = abs_z // grid_length
+            abs_y = y - select_vert[:, 1].min()
+            pos_y = abs_y // grid_length
+            if (pos_z % 2 + pos_y % 2) % 2 == 0:
+                f_dc[idx] = [1, 1, 1]
+        if abs(y) == vertmax:
+            abs_z = z - select_vert[:, 2].min()
+            pos_z = abs_z // grid_length
+            abs_x = x - select_vert[:, 0].min()
+            pos_x = abs_x // grid_length
+            if (pos_z % 2 + pos_x % 2) % 2 == 0:
+                f_dc[idx] = [1, 1, 1]
+        if abs(z) == vertmax:
+            abs_y = y - select_vert[:, 1].min()
+            pos_y = abs_y // grid_length
+            abs_x = x - select_vert[:, 0].min()
+            pos_x = abs_x // grid_length
+            if (pos_y % 2 + pos_x % 2) % 2 == 0:
+                f_dc[idx] = [1, 1, 1]
 
     f_rest = np.zeros((len(xyz), 45))
-    opacities = np.ones((len(xyz), 1))*10
-    scale = np.random.uniform(0.01, 0.1, (len(xyz), 3))
-    scale[:, 2] = 0.01
+    opacities = np.ones((len(xyz), 1)) * 2
+    scale = np.random.uniform(-3, -2, (len(xyz), 3))
+    scale[:, 2] = -3.0
     # generate quaternion based on normal
     rotation, norm1, norm2 = normal2quat(normals)
 
@@ -140,6 +182,77 @@ def gen_ply():
     el = PlyElement.describe(elements, 'vertex')
     PlyData([el]).write("cube_gaussian.ply")
 
+
+def gen_capsule():
+    # generate a cube gaussian spalting ply file
+    mesh = trimesh.creation.capsule(0.2, 0.5)
+    mesh = mesh.subdivide()
+    mesh = mesh.subdivide()
+    mesh = mesh.subdivide()
+    mesh = mesh.subdivide()
+    # save ply file
+    mesh.export("capsule.ply")
+    print("vetices: ", len(mesh.vertices))
+
+    # sample part of the vertex
+    
+    select_id = np.random.choice(len(mesh.vertices), 500)
+    # select_id = [i for i in range(0, 1000, 5)]
+
+    select_vert = mesh.vertices[select_id]
+    select_norm = mesh.vertex_normals[select_id]
+    # add to wis3d
+    print("Add to Wis3D")
+    v3d = Wis3D("dbg", "vis_normal_capsule", "xyz")
+    v3d.add_mesh(mesh, "capsule")
+    # add vertex normal
+    
+    v3d.add_lines(np.array(select_vert), np.array(select_vert + select_norm*0.1), name=f"normals")
+
+    # create gaussian splatting ply
+    print("Create Gaussian Splatting Ply")
+    list_attributes = ["x", "y", "z", "nx", "ny", "nz"]
+    list_attributes += [f"f_dc_{i}" for i in range(3)]
+    # list_attributes += [f"f_rest_{i}" for i in range(45)]
+    list_attributes += ["opacity"]
+    list_attributes += [f"scale_{i}" for i in range(3)]
+    list_attributes += [f"rot_{i}" for i in range(4)]
+
+    dtype_full = [(attr, 'f4') for attr in list_attributes]
+
+    xyz = np.array(select_vert) * 20
+    normals = np.array(select_norm)
+    # generate color in N * [0, 1]
+    # f_dc = np.random.uniform(0, 1, (len(xyz), 3))
+    f_dc = np.zeros((len(xyz), 3))
+
+    max_z = xyz[:, 2].max()
+    min_z = xyz[:, 2].min()
+    for i, z in enumerate(xyz[:, 2]):
+        relative_z = (z - min_z) / (max_z - min_z)
+        color = plt.cm.hsv(relative_z)[:3]
+        f_dc[i] = color
+
+    f_rest = np.zeros((len(xyz), 45))
+    opacities = np.ones((len(xyz), 1))*10
+    scale = np.random.uniform(0.001, 0.01, (len(xyz), 3))
+    scale[:, 2] = 0.01
+    # generate quaternion based on normal
+    rotation, norm1, norm2 = normal2quat(normals)
+
+    # add new axies to vis3d
+    norm1 *= scale[:, 0:1]
+    norm2 *= scale[:, 1:2]
+    v3d.add_lines(select_vert, select_vert + norm1, name=f"norm1")
+    v3d.add_lines(select_vert, select_vert + norm2, name=f"norm2")
+
+    elements = np.empty(xyz.shape[0], dtype=dtype_full)
+    attributes = np.concatenate((xyz, normals, f_dc, opacities, scale, rotation), axis=1)
+    elements[:] = list(map(tuple, attributes))
+    el = PlyElement.describe(elements, 'vertex')
+    PlyData([el]).write("capsule_gaussian.ply")
+
+
 def resize_camera():
     with open("output/cube/cameras.json", "r") as f:
         cameras = json.load(f)  
@@ -151,6 +264,7 @@ def resize_camera():
     
 
 if __name__ == "__main__":
-    gen_ply()
+    gen_cube()
+    # gen_capsule()
     print("Done!")
     # resize_camera()
