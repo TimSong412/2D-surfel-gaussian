17b8f4b5d063aa4181b671bc99e6fc693d6f0838
Ubuntu <ubuntu@ip-172-31-65-131.ec2.internal>
2024-04-14 18:45:06 +0000

    fix name

--- train.py
+++ train.py
@@ -139,8 +139,8 @@ def prepare_output_and_logger(args):
             unique_str=os.getenv('OAR_JOB_ID')
         else:
             unique_str = str(uuid.uuid4())
-        unique_str = time.strftime("%m%d-%H%M%S") + "-2D"
-        args.model_path = os.path.join("./output/", unique_str[0:10])
+        unique_str = time.strftime("%m%d-%H%M%S") 
+        args.model_path = os.path.join("./output/", unique_str[0:10] + "-2D")
         
     # Set up output folder
     print("Output folder: {}".format(args.model_path))
