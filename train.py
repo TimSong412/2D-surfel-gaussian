a57d78fe0f20dde75347945211e1ffba84ac481a
timsong <timsong@seas.upenn.edu>
2024-04-17 23:00:49 +0000

    show xyz grad

--- train.py
+++ train.py
@@ -164,10 +164,10 @@ def training_report(tb_writer, iteration, Ll1, loss, l1_loss, elapsed, testing_i
         tb_writer.add_scalar('train_loss_patches/l1_loss', Ll1.item(), iteration)
         tb_writer.add_scalar('train_loss_patches/total_loss', loss.item(), iteration)
         tb_writer.add_scalar('train_loss_patches/Ld_value', Ld_value.item(), iteration)
-        tb_writer.add_scalar('iter_time', elapsed, iteration)
-        tb_writer.add_scalar('xyz_grad_norm_mean', scene.gaussians.get_xyz.grad.norm(dim=1).mean(), iteration)
+        tb_writer.add_scalar('iter_time', elapsed, iteration) 
         tb_writer.add_scalar('total_points', scene.gaussians.get_xyz.shape[0], iteration)
-        tb_writer.add_scalar('total_quat_err', abs(scene.gaussians._rotation.norm(dim=1)-1).sum(), iteration)
+        tb_writer.add_scalar('mean_opacity', scene.gaussians.get_opacity.mean(), iteration)
+        tb_writer.add_scalar('mean_xyz_grad_mean', scene.gaussians.get_xyz.grad.norm(dim=1).mean(), iteration)
         if hasattr(scene.gaussians, "clone_pts_num"):
             tb_writer.add_scalar('densify/clone', scene.gaussians.clone_pts_num, iteration)
             tb_writer.add_scalar('densify/split', scene.gaussians.split_pts_num, iteration)
