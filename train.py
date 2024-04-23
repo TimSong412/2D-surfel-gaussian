bfa0b0b2f793115aeb133c5fa1d02176e42d3a85
timsong <timsong@seas.upenn.edu>
2024-04-23 00:25:56 +0000

    add depth clamp

--- train.py
+++ train.py
@@ -150,7 +150,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
         # Loss
         gt_image = viewpoint_cam.original_image.cuda()
         Ll1 = l1_loss(image, gt_image)
-        # depth = torch.clamp(depth, 0.1)
+        depth = torch.clamp(depth, 0.1)
         fx = fov2focal(viewpoint_cam.FoVx, viewpoint_cam.image_width)
         fy = fov2focal(viewpoint_cam.FoVy, viewpoint_cam.image_height)
         Ln, depth_norm = norm_loss(ray_P, ray_M, depth, fx, fy, viewpoint_cam.image_width, viewpoint_cam.image_height)
