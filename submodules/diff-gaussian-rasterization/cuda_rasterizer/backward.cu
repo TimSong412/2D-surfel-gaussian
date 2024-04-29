966abef4fc0d96fb3d87fe96f381308cd78796f9
Jaaack <linzx@seas.upenn.edu>
2024-04-29 16:35:09 +0000

    fix previous commit

--- submodules/diff-gaussian-rasterization/cuda_rasterizer/backward.cu
+++ submodules/diff-gaussian-rasterization/cuda_rasterizer/backward.cu
@@ -674,17 +674,6 @@ __global__ void __launch_bounds__(BLOCK_X *BLOCK_Y)
 			dm_dz = 2 * far * near / ((far - near) * intersect_c.z * intersect_c.z);
 			dL_dz = dL_dm * dm_dz;
 
-			// if (blockIdx.x == 30 && blockIdx.y == 50 && threadIdx.x == 8 && threadIdx.y == 8)
-			// {
-			// 	printf("back contributor= %d, dL_domega = %f, dL_dz = %f, dL_dopa = %f, omega = %f, z = %f, alpha = %f, T = %f, Ld = %f\n", contributor, dLd_domega, dL_dz, (T * (dLd_domega - accum_dLd_domega_rec)), omega, intersect_c.z, alpha, T, Wd * (omega * (ndc_m * ndc_m * P_acc + Q2Q_acc - 2 * ndc_m * Q_acc)));
-			// }
-
-			// dL/dp
-			// viewmatrix[2, 0], coloumn major
-			const float dz_dp0 = viewmatrix[2];
-			const float dz_dp1 = viewmatrix[6];
-			const float dz_dp2 = viewmatrix[10];
-
 			atomicAdd(&dL_dmeans3D[global_id].x, dL_dz * dz_dp0);
 			atomicAdd(&dL_dmeans3D[global_id].y, dL_dz * dz_dp1);
 			atomicAdd(&dL_dmeans3D[global_id].z, dL_dz * dz_dp2);
